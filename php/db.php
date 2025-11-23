b0a151c132991622ce33a1daf4b2ff426317ea73
D3l1l4hL <delilah.leutschacher@icloud.com>
2025-11-23 11:59:18 +0100

    Add comment for PDO options in db.php
    Added a comment explaining the PDO options for database connection.

--- php/db.php
+++ php/db.php
@@ -6,7 +6,7 @@ $pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
+//PDO PHP Data Objects - Schnittstelle für DB (SQL-injection-Schutz)
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //SQL Fehler werden sofort und verständlich angezeigt
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // gibt Daten als Arrays zurück ohne doppelte Inhalte
