845e299ece2dfcb4c655da50c346e869f77bfad9
D3l1l4hL <delilah.leutschacher@icloud.com>
2025-11-23 11:47:31 +0100

    Set up database connection in db.php

--- php/db.php
+++ php/db.php
@@ -1,1 +1,20 @@
+<?php
+$host = '127.0.0.1';
+$db   = 'Lumora';      
+$user = 'root';        
+$pass = '';            
+$charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //SQL Fehler werden sofort und verständlich angezeigt
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // gibt Daten als Arrays zurück ohne doppelte Inhalte
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    die('DB connection failed: ' . $e->getMessage());
+}
+?>
