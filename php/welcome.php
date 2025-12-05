93ae973420f4cb420ae06fede5440b9aa8cbe239
D3l1l4hL <delilah.leutschacher@icloud.com>
2025-12-05 19:07:43 +0100

    Update world.php

--- php/welcome.php
+++ php/welcome.php
@@ -108,7 +108,7 @@ $lastLogin   = $user['last_login'] ?? null;
         </div>
 
         <div class="welcome-actions">
-          <a href="../world.php" class="btn btn-primary welcome-play-btn">
+          <a href="../php/world.php" class="btn btn-primary welcome-play-btn">
             Continue your journey
           </a>
         </div>
